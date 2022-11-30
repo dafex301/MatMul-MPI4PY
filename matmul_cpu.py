fdbac8466d0acf95ec3b5ce2b0b2fcb459092b74
Fahrel <fahrel@students.undip.ac.id>
2022-11-30 11:26:49 +0700

    add a guide to run

--- matmul_cpu.py
+++ matmul_cpu.py
@@ -1,3 +1,10 @@
+# =============================================
+#          How to run the program
+# =============================================
+# python matmul_cpu.py 1000
+
+# number after filename is the size of matrix
+
 import numpy as np
 import time
 import sys
@@ -18,3 +25,6 @@ t2 = time.time()
 
 print("\n@================ Result ================@\n", C)
 print("\n@================= Time =================@\n", t2-t1, "\n")
+
+# How to run
+# python matmul_cpu.py 1000
